7e65dcbd20038c478351cc9866a73b5f277688e3
Mingina <mingina.mathea075@gmail.com>
2022-12-08 10:43:01 -0800

    fixing errors task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 	puts(Mystr);
 	return(0);
 }	
