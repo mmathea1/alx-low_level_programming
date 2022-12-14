5ea1dd696e35c31245811b76b9c56594cfeeb7be
Mingina <mingina.mathea075@gmail.com>
2022-12-13 16:37:36 -0800

    added task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
