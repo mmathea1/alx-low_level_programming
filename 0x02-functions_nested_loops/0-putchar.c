28d0af7de7ac3be7f7241254eddcbb2dd9547ad2
Mingina <mingina.mathea075@gmail.com>
2022-12-15 12:59:00 -0800

    betty issues fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <main.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int i;
