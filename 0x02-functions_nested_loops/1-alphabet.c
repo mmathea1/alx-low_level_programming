c03bb32816df4f267a309999ce5cc868e1473974
Mingina <mingina.mathea075@gmail.com>
2022-12-15 13:42:23 -0800

    first attempt

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{	
+
+	return (0);
+}
