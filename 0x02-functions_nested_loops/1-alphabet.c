ba1a7160a3fd1c666c0f6fa52e45245942150132
Mingina Mathea <mingina.mathea075@gmail.com>
2023-10-20 07:43:27 -0700

    fixing linter issues

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - prints the alphabet, in lowercase, followed by a new line
  *
  * Return: Always 0.
  */
@@ -9,12 +9,10 @@
 void print_alphabet(void)
 {
 	char letter;
-	
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
 	
 	}
-	
-	_putchar('\n');	
+	_putchar('\n');
 }
