461cf77595794e85a8a93e46ddd26322e27025ab
Mingina Mathea <mingina.mathea075@gmail.com>
2023-10-24 15:35:51 -0700

    Added t5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number.
+ * @n: character to be checked
+ *
+ * Return: 1 if @n > 0
+ * 0 if @n is 0
+ * -1 if @n is < 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+
+}
